474c037f23f98a9164102882c1745e3fe3d04919
dachiu <dachiu@bu.edu>
2017-09-04 12:30:03 -0400

    simple website for a tae kwon do center

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+//WOW Scroll Spy
+var wow = new WOW({
+    //disabled for mobile
+    mobile: false,
+    offset:       100
+});
+wow.init();
+
+//Contact Form
+
+$('#submit').click(function(){
+
+$.post("assets/php/send.php", $(".contact-form").serialize(),  function(response) {   
+ $('#success').html(response);
+});
+return false;
+
+});
+
+$(document).ready(function() {
+	$('.fancybox-media').fancybox({
+		openEffect  : 'none',
+		closeEffect : 'none',
+		helpers : {
+			media : {}
+		}
+	});
+});
+
